5d766ce35ddc14e0283e7999119bae7a73a374f0
dareck1 <darebinao13@gmail.com>
2023-03-21 00:49:25 +0000

    Assign a random number to the variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - print sizeof
+ * main - print sizes
  * Return: 0
  */
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n" (unsigned long)sizeof(char));
-	printf("Size of an int is: %lu byte(s)\n" (unsigned long)sizeof(int));
-	printf("Size of a  long int is: %lu byte(s)\n" (unsigned long)sizeof(long int));
-	printf("Size of a long long is: %lu byte(s)\n" (unsigned long)sizeof(long long));
-	printf("Size of a float is: %lu byte(s)\n" (unsigned long)sizeof(float));
-	return (0);
-}	
+	printf("The size of a char is: %lu byte(s)\n" (unsigned long)sizeof(char));
+	printf("The size of an int is: %lu byte(s)\n" (unsigned long)sizeof(int));
+	printf("The size of a long int is: %lu byte(s)\n" (unsigned long)sizeof(long int));
+	printf("The size of a long long int is: %lu byte(s)\n" (unsigned long)sizeof(long long));
+	printf("The size of a float is: %lu byte(s)\n" (unsigned long)sizeof(float));
+	return (o);
+}
