113165a3e5bfc684df787238bc100e5e43264405
dareck1 <darebinao13@gmail.com>
2023-03-24 02:51:37 +0000

    Infinit loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,9 +12,8 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
-	/**
-	*while (i < 10)
+	
+	/**while (i < 10)
 	*{
 	*	putchar(i);
 	*}
@@ -22,5 +21,4 @@ int main(void)
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
-
 }
