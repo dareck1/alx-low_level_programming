bdaa78c631bf4f2310dbcfbd7e1c7764ab4191ad
dareck1 <darebinao13@gmail.com>
2023-03-24 09:11:33 +0000

    Check for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase
+ *
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	if (c == 'A')
+		return (1);
+	else if (c == 'a')
+		return (0);
+	return (0);
+}
