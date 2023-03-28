87643377ea4d350a2bacb36a2d4e19b045c55c42
dareck1 <darebinao13@gmail.com>
2023-03-28 21:36:11 +0000

    Swap value

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swape value of integers
+ *
+ * @a: parameter a
+ * @b: parameter b
+ *
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
