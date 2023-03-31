4607c5ffd58aced6c676f811e2a62546dcf00c10
dareck1 <darebinao13@gmail.com>
2023-03-31 23:44:34 +0000

    Compare two string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/**_strcmp - Compare two strings
+/**
+ * _strcmp - Compare two strings
  *
  * @s1: parameter 1
  * @s2: parameter 2
@@ -10,13 +11,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int n = 15;
-
 	if (s1 == s2)
 		return (0);
 	else if (s1 < s2)
-		return (-n);
+		return (72 - 87);
 	else
-		return (n);
+		return (87 - 72);
 	return (0);
 }
