fa1f76348e4814aaddbea163246fe000c6e3936e
dareck1 <darebinao13@gmail.com>
2023-04-02 13:51:30 +0000

    Print all single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this function will print all single difgit
+ *
+ * Rturn: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
