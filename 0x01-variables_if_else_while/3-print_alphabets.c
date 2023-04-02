a6e14464f5308b6579acfd24108dfb4792a632bd
dareck1 <darebinao13@gmail.com>
2023-04-02 07:32:17 +0000

    Print the alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int i;
 
-	for(i = 97; i <= 122; i++)
+	for (i = 97; i <= 122; i++)
 		putchar(i);
-	for (i = 65; i<= 90; i++)
+	for (i = 65; i <= 90; i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
