d58f906b5757f84edf65da354bd28607dc3fe8c2
dareck1 <darebinao13@gmail.com>
2023-04-02 17:22:48 +0000

    Print the numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This function print all digit numbers of base 10
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	       putchar(i);
+	putchar('\n');
+	return (0);
+}
