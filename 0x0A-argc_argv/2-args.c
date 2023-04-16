7a0a7ce9219d7d0b1e20e6e94608e1e77063f126
dareck1 <darebinao13@gmail.com>
2023-04-16 02:39:04 +0000

    argc, argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,6 +3,9 @@
 /**
  * main - print all the argument receive
  *
+ * @argc: para0
+ * @argv: para1
+ *
  * Return: 0
  */
 
@@ -10,7 +13,7 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for(i = 0; i < argc; i++)
+	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
 	return (0);
 }
