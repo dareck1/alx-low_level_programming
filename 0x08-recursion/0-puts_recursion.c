71a0eaa7ee2997356f92c52c88e59ef8dad7ae29
dareck1 <darebinao13@gmail.com>
2023-07-05 21:38:10 +0000

    print string whith recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts_recusion - print a string
+ *
+ * @s: string will be print
+ *
+ * Return: anything
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
