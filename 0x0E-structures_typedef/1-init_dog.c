1721702748ab908499cb6f0444237b863d81d017
dareck1 <darebinao13@gmail.com>
2023-07-19 12:36:22 +0000

    initiallize sltruct dogl

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - initialize a structer
+ *
+ * @d: pointer pointing to struct dog
+ * @name: element1
+ * @age: element2
+ * @owner: element3
+ *
+ * Return: anything
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
