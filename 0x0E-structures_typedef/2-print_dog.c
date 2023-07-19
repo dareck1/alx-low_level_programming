4ad8a7d864deb9541302fc6808f60eee5433c9d6
dareck1 <darebinao13@gmail.com>
2023-07-19 12:33:52 +0000

    initiallize sltruct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - print struct dog
+ *
+ * @d: pointer to struct dog
+ *
+ * Return: anything
+ */
+
+void print_dog(struct dog *d)
+{
+	
