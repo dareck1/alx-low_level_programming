ae4489d2f88d18383fa9d6bb14bfc567885b8e05
dareck1 <darebinao13@gmail.com>
2023-07-20 08:49:41 +0000

    new name of dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,25 +11,14 @@
  * Description: Define a new type struct dog with the following elements:
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-
-
-/**
- * struct dog_t - structure named dog_t
- *
- * @name: name od dog                            * @age: age of dog                              * @owner: owner                                 *                                               * Description: Define a new type struct dog with the following elements:                        */
-
-typedef struct dog
-{
-	char *name;
-	float age;                                      char *owner;
 } dog_t;
+
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);                               void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);                                             void free_dog(dog_t *d);
