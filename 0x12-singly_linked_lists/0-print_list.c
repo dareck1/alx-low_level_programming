a5e493e20f164b08a54126797067b464f1320c69
dareck1 <darebinao13@gmail.com>
2023-07-28 21:16:01 +0000

    that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,40 +3,6 @@
 #include <stdlib.h>
 
 /**
- * isEmpty - check the list if is empty
- *
- * @h: list to check
- *
- * Return: 0 or 1
- */
-
-int isEmpty(const list_t *h)
-{
-	return (h == NULL);
-}
-
-/**
- * lenList - length of a string
- *
- * @h: lists
- *
- * Return: length
- */
-int lenList(const list_t *h)
-{
-	int length = 0;
-
-	if (isEmpty(h))
-		return (length);
-	while (h)
-	{
-		length++;
-		h = h->next;
-	}
-	return (length);
-}
-
-/**
  * print_list - function that prints all the elements of a list_t list.
  *
  * @h: list
@@ -47,6 +13,7 @@ int lenList(const list_t *h)
 size_t print_list(const list_t *h)
 {
 	const list_t *temp = h;
+	size_t len = 0;
 
 	while (temp != NULL)
 	{
@@ -54,7 +21,7 @@ size_t print_list(const list_t *h)
 			printf("[%d] (nil)\n", 0);
 		printf("[%d] %s\n", temp->len, temp->str);
 		temp = temp->next;
+		len++;
 	}
-	return (lenList(h));
-	printf("\n");
+	return (len);
 }
