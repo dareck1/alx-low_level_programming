ad9eaa049a1ba2dcfa0c51cf41f99f122d3ee436
dareck1 <darebinao13@.com>
2023-09-01 01:17:51 -0700

    function that returns the natural square root of a number.

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string
+ *
+ * @s: string will be printed
+ *
+ * Return: anything
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
