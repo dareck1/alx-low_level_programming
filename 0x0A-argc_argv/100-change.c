b0cd0a62c40bb0765b35625c788f00d6b3406637
dareck1 <darebinao13@.com>
2023-09-06 07:55:09 -0700

    argv argc

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -  prints the minimum number of coins to make change for an amount
+ *
+ * @argc: size of
+ * @argv: argument passed
+ *
+ * Return: integer
+ */
+
+int main(int argc, char **argv)
+{
+
