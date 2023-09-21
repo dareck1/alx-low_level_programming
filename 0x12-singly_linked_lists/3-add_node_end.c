71dd2db2b2133318431994808bb1c780211455c3
dareck1 <darebinao13@gmail.com>
2023-09-21 22:56:14 +0000

    singly linked list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node_end - add node at the end of list
+ *
+ * @head: head pointer to the lst
+ * @str: string to put in the new node
+ *
+ * Return: address of the new element
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *element, *temp;
+	size_t len = strlen(str);
+
+	element = malloc(sizeof(list_t));
+	if (element == NULL)
+		return (NULL);
+	Li->str = strdup(str);
+	Li->len = len;
+	Li->next = NULL;
+	temp = *head;
+	if (*head == NULL)
+	{
+		*head = element;
+		return (element);
+	}
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = element;
+	return (element);
+}
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * free_list - free all the nodes
+ *
+ * @head: pointer to the list
+ *
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+	head = NULL;
+}
