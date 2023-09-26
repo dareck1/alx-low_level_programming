a0ae34084b02ec3e2849a64d01461986b02da8b7
dareck1 <darebinao13@gmail.com>
2023-09-26 19:49:26 +0000

    array range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,5 +12,16 @@
  */
 
 int *array_range(int min, int max)
-{       
-        int *ta
+{
+	int *tab, size, i;
+
+	size = max - min + 1;
+	if (min > max)
+		return (NULL);
+	tab = malloc(sizeof(int) * size);
+	if (tab == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		tab[i] = min + i;
+	return (tab);
+}
